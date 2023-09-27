e81b8b33d52dbbf5c6e0dba04a303014e766b6b4
2022525 <2022525@student.cct.ie>
2023-09-27 14:05:42 +0100

    Initial commit

---
+++ Forward2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package footballfantasysetup;
+
+/**
+ *
+ * @author hasan
+ */
+public class Forward2 extends Player {
+    int numGoalsscored;
+
+    public Forward2(int numGoalsscored, String fname, String lname, String country, int age, String position) {
+        super(fname, lname, country, age, position);
+        this.numGoalsscored = numGoalsscored;
+    }
+
+   
+    
+}
